90a2e8dc1aa6f35c7cc1be876d9aa7b52cd1b270
Zoommod <vitor.sacramento09@gmail.com>
2024-03-13 09:26:34 -0300

    UPDATE: alterações gerais.

--- project_scorpio/urls.py
+++ project_scorpio/urls.py
@@ -1,22 +1,15 @@
-"""
-URL configuration for project_scorpio project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from django.urls import path
+from user.views import CustomPasswordChangeView, dashboard_view
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', RedirectView.as_view(pattern_name='login', permanent=False)),  # Redireciona para a página de login
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('change-password/', CustomPasswordChangeView.as_view(), name='change_password'),
+    path('dashboard/', dashboard_view, name='dashboard'),
 ]
+
